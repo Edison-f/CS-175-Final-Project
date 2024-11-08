0cad0baa962b628e63e5b90482e26ee76567dc3a
Edison-f <osgoodbacon@gmail.com>
2024-11-08 12:43:22 -0800

    Add grade calculation

--- app/src/main/java/edu/sjsu/android/cs175finalproject/Course.java
+++ app/src/main/java/edu/sjsu/android/cs175finalproject/Course.java
@@ -17,14 +17,24 @@ public class Course {
     private String name;
     private HashMap<String, ArrayList<Assignment>> assignments; // Group, Arraylist of assignments within group
     private ArrayList<String> groups; // Want a group since HashMap not necessarily sorted
+    private HashMap<String, Double> groupWeights;
+    private int[] thresholds; // Grade thresholds, (inclusive)
+    private boolean roundUp; // Is the professor expected to round up to nearest percent
+    private static String[] gradeLetters = new String[]{"A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F+", "F", "F-"};
 
     public Course() {
         grade = 100;
         name = "Placeholder";
         assignments = new HashMap<>();
         groups = new ArrayList<>();
+        groupWeights = new HashMap<>();
         groups.add("Default");
+        groupWeights.put("Default", 1.0);
         assignments.put("Default", new ArrayList<>());
+                            // To make a grade unachievable set to value -1
+                            // If no threshold met, give F
+                            //  A+, A,  A-, B+, B,  B-, C+, C,  C-, D+,  D, D-, F+, F, F-
+        thresholds = new int[] {98, 92, 90, 88, 82, 80, 78, 72, 70, 68, 62, 60, -1, -1, -1};
     }
 
     public String getName() {
@@ -32,17 +42,42 @@ public class Course {
     }
 
     public String getNumericalGrade() {
-        DecimalFormat df = new DecimalFormat("0.00")
+        DecimalFormat df = new DecimalFormat("0.00");
         return df.format(grade);
     }
 
     public String getLetterGrade() {
-        // TODO
-        return "TODO";
+        for (int i = 0; i < thresholds.length; i++) {
+            if(grade > thresholds[i] && thresholds[i] != -1) {
+                return gradeLetters[i];
+            }
+        }
+        return "F";
     }
 
     public void recalculate() {
-        // TODO
+        grade = 0;
+        for (String group :
+                groups) {
+            ArrayList<Assignment> arr = assignments.get(group);
+            if(arr == null) {
+                continue;
+            }
+            double total = 0;
+            for (Assignment a :
+                    arr) {
+                total += a.pointsPossible;
+            }
+            for (Assignment a :
+                    arr) {
+
+                Double weight = groupWeights.get(group);
+                if(weight == null) {
+                    weight = 1.0;
+                }
+                grade += ((a.grade / a.pointsPossible) / total) * weight;
+            }
+        }
     }
 
     public void addGroup(String group) {
@@ -52,6 +87,7 @@ public class Course {
 
     public class Assignment {
         public double grade;
+        public double pointsPossible;
         public String group;
     }
 }
